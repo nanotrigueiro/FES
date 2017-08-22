9ebba83b958dabb9c99b4196700fe05c09ff8886
RENATA ALBUQUERQUE <rca14@cin.ufpe.br>
2017-08-22 01:14:14 +0000

    static

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
 class User < ActiveRecord::Base
-    validates :user.name, content length: { minimum: 2 }
-    #validates user.email length: { minimum: 5 }
-    #validates user.password length: { minimum: 6 }
+    
 end
